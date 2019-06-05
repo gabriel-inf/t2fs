1d3fbda1c13090f4044fc8a323768ac9dc999360
Gabriel Stepien <gabriel.stepien@inf.ufrgs.br>
2019-06-05 04:40:52 +0000

    Update include/data.h
    Co-Authored-By: lauracorssac <lauracorssac@hotmail.com>

--- include/data.h
+++ include/data.h
@@ -11,7 +11,7 @@ typedef struct{
     unsigned int generalBlocksBegin;
     unsigned int numberOfBlocks;
     unsigned int bitmap_sector;
-    unsigned int bitmap_size;
+    unsigned int bitmap_size; // tamanho em bytes do bitmap
 
 }SuperBloco;
 
