4ae03badd6f54907ad7fe2ec9266a0d4a5871b62
Laura Corssac <lauracorssac@hotmail.com>
2019-06-14 18:10:26 -0300

    if test fails, it is wrong not the code

--- teste/test_hashtable.c
+++ teste/test_hashtable.c
@@ -285,14 +285,19 @@ int test_read_dir() {
     opened_dir->current_entry_index = 0;
 
     assert(SUCCESS_CODE == readdir2(1, &dirent2));
-    assert(opened_dir->current_entry_index == 3);
+    assert(opened_dir->current_entry_index == 2);
     assert(dirent2.fileType == carissimi_dir_entry.fileType);
     assert(dirent2.fileSize == carissimi_dir_entry.fileSize);
     assert(0 == strcmp(dirent2.name, carissimi_dir_entry.name));
 
+    hashArray[0].valid = 0;
+    hashArray[1].valid = 0;
+    hashArray[2].valid = 0;
+    opened_dir->current_entry_index = 0;
+
+    assert(SUCCESS_CODE != readdir2(1, &dirent2));
 
 	printf("TODOS OS TESTES READ DIR PASSARAM\n");
-	
     
 }
 
